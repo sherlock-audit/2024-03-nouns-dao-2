e2d88c7ab14006a5a47c0fcfa45a36eca356dfb9
sherlock-admin <dev@sherlock.xyz>
2024-03-04 14:45:31 +0000

    Copy nouns-monorepo @ 9c8775fdcad2cd6114cea444c57608b715523a45

---
+++ nouns-monorepo/packages/nouns-contracts/contracts/client-incentives/NounsClientToken.sol
@@ -0,0 +1,101 @@
+// SPDX-License-Identifier: GPL-3.0
+
+/*********************************
+ * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
+ * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
+ * ░░░░░░█████████░░█████████░░░ *
+ * ░░░░░░██░░░████░░██░░░████░░░ *
+ * ░░██████░░░████████░░░████░░░ *
+ * ░░██░░██░░░████░░██░░░████░░░ *
+ * ░░██░░██░░░████░░██░░░████░░░ *
+ * ░░░░░░█████████░░█████████░░░ *
+ * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
+ * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
+ *********************************/
+
+pragma solidity ^0.8.19;
+
+import { ERC721Upgradeable } from '@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol';
+import { OwnableUpgradeable } from '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
+import { INounsClientTokenTypes } from './INounsClientTokenTypes.sol';
+import { INounsClientTokenDescriptor } from './INounsClientTokenDescriptor.sol';
+
+contract NounsClientToken is INounsClientTokenTypes, ERC721Upgradeable, OwnableUpgradeable {
+    /// @custom:storage-location erc7201:nouns.nounsclienttoken
+    struct NounsClientTokenStorage {
+        uint32 nextTokenId;
+        address descriptor;
+        mapping(uint32 => ClientMetadata) clientMetadata;
+    }
+
+    /// @dev This is a ERC-7201 storage location, calculated using:
+    /// @dev keccak256(abi.encode(uint256(keccak256("nouns.nounsclienttoken")) - 1)) & ~bytes32(uint256(0xff));
+    bytes32 public constant NounsClientTokenStorageLocation =
+        0xb5ff9f2ad3ce9c5f981fca1b696d577b6a7f3491afe19108b82d4fbb7f611600;
+
+    event ClientRegistered(uint32 indexed clientId, string name, string description);
+    event ClientUpdated(uint32 indexed clientId, string name, string description);
+
+    constructor() initializer {}
+
+    function initialize(address owner, address descriptor_) public initializer {
+        NounsClientTokenStorage storage $ = _getNounsClientTokenStorage();
+
+        __ERC721_init('Nouns Client Token', 'NOUNSCLIENT');
+        _transferOwnership(owner);
+        $.nextTokenId = 1;
+        $.descriptor = descriptor_;
+    }
+
+    function registerClient(string calldata name, string calldata description) public virtual returns (uint32) {
+        NounsClientTokenStorage storage $ = _getNounsClientTokenStorage();
+        uint32 tokenId = $.nextTokenId;
+        $.nextTokenId++;
+        _mint(msg.sender, tokenId);
+        $.clientMetadata[tokenId] = ClientMetadata(name, description);
+
+        emit ClientRegistered(tokenId, name, description);
+
+        return tokenId;
+    }
+
+    function updateClientMetadata(uint32 tokenId, string calldata name, string calldata description) public {
+        NounsClientTokenStorage storage $ = _getNounsClientTokenStorage();
+
+        require(ownerOf(tokenId) == msg.sender, 'NounsClientToken: not owner');
+        $.clientMetadata[tokenId] = ClientMetadata(name, description);
+
+        emit ClientUpdated(tokenId, name, description);
+    }
+
+    function setDescriptor(address descriptor_) public onlyOwner {
+        NounsClientTokenStorage storage $ = _getNounsClientTokenStorage();
+        $.descriptor = descriptor_;
+    }
+
+    function clientMetadata(uint32 tokenId) public view returns (ClientMetadata memory) {
+        NounsClientTokenStorage storage $ = _getNounsClientTokenStorage();
+        return $.clientMetadata[tokenId];
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        NounsClientTokenStorage storage $ = _getNounsClientTokenStorage();
+        return INounsClientTokenDescriptor($.descriptor).tokenURI(tokenId, $.clientMetadata[uint32(tokenId)]);
+    }
+
+    function descriptor() public view returns (address) {
+        NounsClientTokenStorage storage $ = _getNounsClientTokenStorage();
+        return $.descriptor;
+    }
+
+    function nextTokenId() public view returns (uint32) {
+        NounsClientTokenStorage storage $ = _getNounsClientTokenStorage();
+        return $.nextTokenId;
+    }
+
+    function _getNounsClientTokenStorage() private pure returns (NounsClientTokenStorage storage $) {
+        assembly {
+            $.slot := NounsClientTokenStorageLocation
+        }
+    }
+}
---
+++ nouns-monorepo/packages/nouns-contracts/test/foundry/DAOUpgrade/DAOUpgradeMainnetFork.t.sol
@@ -0,0 +1,208 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.19;
+
+import 'forge-std/Test.sol';
+import { Strings } from '@openzeppelin/contracts/utils/Strings.sol';
+import { NounsDAOLogicV4 } from '../../../contracts/governance/NounsDAOLogicV4.sol';
+import { ProposeDAOUpgradeMainnet } from '../../../script/DAOUpgrade/ProposeDAOUpgradeMainnet.s.sol';
+import { NounsToken } from '../../../contracts/NounsToken.sol';
+import { INounsDAOLogic } from '../../../contracts/interfaces/INounsDAOLogic.sol';
+import { NounsDAOTypes } from '../../../contracts/governance/NounsDAOInterfaces.sol';
+
+abstract contract DAOUpgradeMainnetForkBaseTest is Test {
+    address public constant NOUNDERS = 0x2573C60a6D127755aA2DC85e342F7da2378a0Cc5;
+    address public constant WHALE = 0x83fCFe8Ba2FEce9578F0BbaFeD4Ebf5E915045B9;
+    NounsToken public nouns = NounsToken(0x9C8fF314C9Bc7F6e59A9d9225Fb22946427eDC03);
+    INounsDAOLogic public constant NOUNS_DAO_PROXY_MAINNET = INounsDAOLogic(0x6f3E6272A167e8AcCb32072d08E0957F9c79223d);
+    address public constant CURRENT_DAO_IMPL = 0xe3caa436461DBa00CFBE1749148C9fa7FA1F5344;
+
+    address proposerAddr = vm.addr(0xbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb);
+    address origin = makeAddr('origin');
+    address newLogic;
+
+    function setUp() public virtual {
+        vm.createSelectFork(vm.envString('RPC_MAINNET'), 19127187);
+
+        // Get votes
+        vm.prank(NOUNDERS);
+        nouns.delegate(proposerAddr);
+        vm.roll(block.number + 1);
+
+        vm.deal(address(NOUNS_DAO_PROXY_MAINNET), 100 ether);
+        vm.fee(50 gwei);
+        vm.txGasPrice(50 gwei);
+    }
+
+    function propose(
+        address target,
+        uint256 value,
+        string memory signature,
+        bytes memory data
+    ) internal returns (uint256 proposalId) {
+        vm.prank(proposerAddr);
+        address[] memory targets = new address[](1);
+        targets[0] = target;
+        uint256[] memory values = new uint256[](1);
+        values[0] = value;
+        string[] memory signatures = new string[](1);
+        signatures[0] = signature;
+        bytes[] memory calldatas = new bytes[](1);
+        calldatas[0] = data;
+        proposalId = NOUNS_DAO_PROXY_MAINNET.propose(targets, values, signatures, calldatas, 'my proposal');
+    }
+
+    function propose(
+        address target,
+        uint256 value,
+        string memory signature,
+        bytes memory data,
+        uint32 clientId
+    ) internal returns (uint256 proposalId) {
+        vm.prank(proposerAddr);
+        address[] memory targets = new address[](1);
+        targets[0] = target;
+        uint256[] memory values = new uint256[](1);
+        values[0] = value;
+        string[] memory signatures = new string[](1);
+        signatures[0] = signature;
+        bytes[] memory calldatas = new bytes[](1);
+        calldatas[0] = data;
+        proposalId = NOUNS_DAO_PROXY_MAINNET.propose(targets, values, signatures, calldatas, 'my proposal', clientId);
+    }
+
+    function voteAndExecuteProposal(uint256 proposalId) internal {
+        NounsDAOTypes.ProposalCondensedV2 memory propInfo = NOUNS_DAO_PROXY_MAINNET.proposals(proposalId);
+
+        vm.roll(propInfo.startBlock + 1);
+        vm.prank(proposerAddr, origin);
+        NOUNS_DAO_PROXY_MAINNET.castRefundableVote(proposalId, 1);
+        vm.prank(WHALE, origin);
+        NOUNS_DAO_PROXY_MAINNET.castRefundableVote(proposalId, 1);
+
+        vm.roll(propInfo.endBlock + 1);
+        NOUNS_DAO_PROXY_MAINNET.queue(proposalId);
+
+        propInfo = NOUNS_DAO_PROXY_MAINNET.proposals(proposalId);
+        vm.warp(propInfo.eta + 1);
+        NOUNS_DAO_PROXY_MAINNET.execute(proposalId);
+    }
+}
+
+contract DAOUpgradeMainnetForkTest is DAOUpgradeMainnetForkBaseTest {
+    function setUp() public override {
+        super.setUp();
+
+        // Deploy the latest DAO logic
+        vm.setEnv('DEPLOYER_PRIVATE_KEY', '0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+        newLogic = address(new NounsDAOLogicV4());
+
+        // Propose the upgrade
+        vm.setEnv('PROPOSER_KEY', '0xbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb');
+        vm.setEnv('DAO_V3_IMPL', Strings.toHexString(uint160(newLogic), 20));
+        vm.setEnv('PROPOSAL_DESCRIPTION_FILE', 'test/foundry/DAOUpgrade/proposal-description.txt');
+        uint256 proposalId = new ProposeDAOUpgradeMainnet().run();
+
+        // Execute the upgrade
+        voteAndExecuteProposal(proposalId);
+    }
+
+    function test_daoUpgradeWorked() public {
+        assertTrue(CURRENT_DAO_IMPL != NOUNS_DAO_PROXY_MAINNET.implementation());
+        assertEq(newLogic, NOUNS_DAO_PROXY_MAINNET.implementation());
+    }
+
+    function test_givenRecentBitPacking_creationBlockAndProposalIdValuesAreLegit() public {
+        NounsDAOTypes.ProposalCondensed memory prop = NOUNS_DAO_PROXY_MAINNET.proposalsV3(493);
+
+        assertEq(prop.id, 493);
+        assertEq(prop.creationBlock, 19093670);
+        assertEq(prop.creationTimestamp, 0);
+
+        prop = NOUNS_DAO_PROXY_MAINNET.proposalsV3(474);
+
+        assertEq(prop.id, 474);
+        assertEq(prop.creationBlock, 18836862);
+        assertEq(prop.creationTimestamp, 0);
+    }
+
+    function test_creationTimestampAndBlock_setOnNewProposals() public {
+        assertTrue(block.timestamp > 0);
+        assertTrue(block.number > 0);
+        uint256 proposalId = propose(address(NOUNS_DAO_PROXY_MAINNET), 0, '', '');
+
+        NounsDAOTypes.ProposalCondensed memory prop = NOUNS_DAO_PROXY_MAINNET.proposalsV3(proposalId);
+
+        assertEq(prop.creationTimestamp, block.timestamp);
+        assertEq(prop.creationBlock, block.number);
+    }
+
+    function test_adminFunctions_workUsingTheNewFallbackDesign() public {
+        uint256 currentForkPeriod = NOUNS_DAO_PROXY_MAINNET.forkPeriod();
+        uint256 expectedForkPeriod = currentForkPeriod + 1;
+
+        uint256 proposalId = propose(
+            address(NOUNS_DAO_PROXY_MAINNET),
+            0,
+            '_setForkPeriod(uint256)',
+            abi.encode(expectedForkPeriod)
+        );
+        voteAndExecuteProposal(proposalId);
+
+        assertEq(expectedForkPeriod, NOUNS_DAO_PROXY_MAINNET.forkPeriod());
+
+        uint256 currentVotingDelay = NOUNS_DAO_PROXY_MAINNET.votingDelay();
+        uint256 expectedVotingDelay = currentVotingDelay - 1;
+
+        proposalId = propose(
+            address(NOUNS_DAO_PROXY_MAINNET),
+            0,
+            '_setVotingDelay(uint256)',
+            abi.encode(expectedVotingDelay)
+        );
+        voteAndExecuteProposal(proposalId);
+
+        assertEq(expectedVotingDelay, NOUNS_DAO_PROXY_MAINNET.votingDelay());
+    }
+
+    function test_clientId_savedOnProposals() public {
+        uint32 expectedClientId = 42;
+        uint256 proposalId = propose(address(NOUNS_DAO_PROXY_MAINNET), 0, '', '', expectedClientId);
+
+        NounsDAOTypes.ProposalForRewards[] memory propsData = NOUNS_DAO_PROXY_MAINNET.proposalDataForRewards(
+            proposalId,
+            proposalId,
+            new uint32[](0)
+        );
+        assertEq(expectedClientId, propsData[0].clientId);
+    }
+
+    function test_clientId_savedOnVotes() public {
+        uint256 proposalId = propose(address(NOUNS_DAO_PROXY_MAINNET), 0, '', '');
+        NounsDAOTypes.ProposalCondensedV2 memory propInfo = NOUNS_DAO_PROXY_MAINNET.proposals(proposalId);
+        vm.roll(propInfo.startBlock + 1);
+
+        uint32 clientId1 = 42;
+        uint32 clientId2 = 142;
+
+        vm.prank(proposerAddr, origin);
+        NOUNS_DAO_PROXY_MAINNET.castRefundableVote(proposalId, 1, clientId1);
+        vm.prank(WHALE, origin);
+        NOUNS_DAO_PROXY_MAINNET.castRefundableVote(proposalId, 1, clientId2);
+
+        uint32[] memory clientIds = new uint32[](2);
+        clientIds[0] = clientId1;
+        clientIds[1] = clientId2;
+
+        NounsDAOTypes.ProposalForRewards[] memory propsData = NOUNS_DAO_PROXY_MAINNET.proposalDataForRewards(
+            proposalId,
+            proposalId,
+            clientIds
+        );
+        NounsDAOTypes.ClientVoteData[] memory voteData = propsData[0].voteData;
+
+        assertEq(voteData[0].txs, 1);
+        assertEq(voteData[0].votes, nouns.getCurrentVotes(proposerAddr));
+        assertEq(voteData[1].txs, 1);
+        assertEq(voteData[1].votes, nouns.getCurrentVotes(WHALE));
+    }
+}
